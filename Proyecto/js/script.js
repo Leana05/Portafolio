4bbfb49377b2813a27a34574f89ffe6dbcb1a7d6
Leana05 <133674504+Leana05@users.noreply.github.com>
2023-11-17 13:47:51 -0500

    Add files via upload

---
+++ Proyecto/js/script.js
@@ -0,0 +1,18 @@
+/*Página principal*/
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  centeredSlides: true,
+  loop: true,
+  spaceBetween: 30,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  breakpoints: {
+    991: {
+      slidesPerView: 2
+    },
+  },
+});
+
